ce56518cdad1a4b62fb76425b97ec6b83ce02004
vagisha <vagisha.jain@tothenew.com>
2022-04-18 11:41:28 +0530

    packer-ami terraform setup

---
+++ main.tf
@@ -0,0 +1,56 @@
+module "db" {
+  source        = "./modules/db/"
+  vpc_asg       = module.network.vpc_id_all
+  sec_group_rds = module.network.security_group_id_rds
+  subnet_rds    = module.network.public_sn_asg
+}
+
+module "asg" {
+  source        = "./modules/asg/"
+  subnet_asg    = module.network.public_sn_asg
+  sec_group_asg = module.network.security_group_id_asg
+  rds_point     = module.db.rds_endpoint
+  depends_on    = [module.db.rds_endpoint]
+  target_gp     = module.alb.tg
+  img_id        = data.aws_ami.packer_ami.id
+}
+
+module "network" {
+  source = "./modules/network/"
+}
+
+module "alb" {
+  source        = "./modules/alb/"
+  vpc_alb       = module.network.vpc_id_all
+  sec_group_alb = module.network.security_group_id_asg
+  subnet_alb    = module.network.public_sn_asg
+}
+
+/*
+module "monitoring" {
+  source = "./modules/monitoring/"
+  lb_dns     = module.alb.dns
+  depends_on    = [module.alb.dns]
+  subnet_monitoring_instance = module.network.public_sn_asg[0]
+  vpc_security_group_monitoring = module.network.security_group_id_asg
+}
+*/
+
+data "aws_ami" "packer_ami" {
+  most_recent = true
+  owners = ["self"]
+  name_regex = "^packer*"
+
+  # filter {
+  #   name   = "packer-*"
+  #   values = ["available"]
+  # }
+}
+
+output "dns" {
+  value = module.alb.dns
+}
+
+output "rds_endpoint" {
+  value = module.db.rds_endpoint
+}
---
+++ modules/asg/main.tf
@@ -0,0 +1,139 @@
+locals {
+  name = "group-asg"
+}
+
+# generate key-pair for launch template
+resource "tls_private_key" "dev_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "instance-key"
+  public_key = tls_private_key.dev_key.public_key_openssh
+
+  provisioner "local-exec" {    
+    command = "echo '${tls_private_key.dev_key.private_key_pem}' > ./instance-key.pem"
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 400 ./instance-key.pem"
+  }
+}
+
+resource "aws_iam_role" "drupal_access_role" {
+  name               = "drupal_access_role"
+  assume_role_policy = file("./modules/asg/policies/assume_role_policy.json")
+}
+
+resource "aws_iam_instance_profile" "drupal-grp_profile" {
+  name = "drupal-grp_profile"
+  role = aws_iam_role.drupal_access_role.name
+}
+
+# resource "aws_iam_policy" "cloudwatchagentserver-policy" {
+#   name        = "cloudwatchagentserver-policy"
+#   description = "cloudwatch agent server policy"
+#   policy      = file("./modules/asg/policies/CloudwatchAgentServerPolicy.json")
+# }
+
+# resource "aws_iam_policy_attachment" "cloudwatchagentserver-attach" {
+#   name       = "cloudwatchagentserver-attachment"
+#   roles      = [aws_iam_role.drupal_access_role.name]
+#   policy_arn = aws_iam_policy.cloudwatchagentserver-policy.arn
+# }
+
+data "aws_iam_policy" "CloudWatchAgentPolicy" {
+  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_policy_attachment" "cloudwatch-agent-server-policy-attach" {
+  name =  "cloudwatch-agent-server-policy-attach"
+  roles       = [aws_iam_role.drupal_access_role.name]
+  policy_arn = data.aws_iam_policy.CloudWatchAgentPolicy.arn
+}
+
+resource "aws_iam_policy" "cw-s3-policy" {
+  name        = "cw-s3-policy"
+  description = "cloudwatch agent s3 server policy"
+  policy      = file("./modules/asg/policies/s3-policy.json")
+}
+
+resource "aws_iam_policy_attachment" "cw-s3-attach" {
+  name       = "cw-s3-attachment"
+  roles      = [aws_iam_role.drupal_access_role.name]
+  policy_arn = aws_iam_policy.cw-s3-policy.arn
+}
+
+
+module "aws_autoscaling_group" {
+  #source = "git@github.com:terraform-aws-modules/terraform-aws-autoscaling.git?ref=v4.1.0"
+
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "4.1.0"
+
+  # Autoscaling group
+  name = "group-testing"
+
+  min_size                  = 1
+  max_size                  = 5
+  desired_capacity          = 2
+  wait_for_capacity_timeout = "5m"
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = var.subnet_asg
+  security_groups           = [var.sec_group_asg]
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      min_healthy_percentage = 50
+    }
+  }
+
+  # Launch template
+  lt_name     = "foobar"
+  description = "Complete launch template example"
+  use_lt      = true
+  create_lt   = true
+
+  image_id      = var.img_id
+  instance_type = "t2.micro"
+  key_name      = "instance-key"
+  iam_instance_profile_arn = aws_iam_instance_profile.drupal-grp_profile.arn
+  #user_data_base64 = base64encode(local.user_data)
+  user_data_base64 = base64encode(templatefile("${path.module}/userdata.sh", {
+    rds_endpt = var.rds_point
+  }))
+
+  target_group_arns = var.target_gp
+
+  health_check_grace_period = 300
+
+  tags = [
+    {
+      key                 = "Project"
+      value               = "terraform_drupal"
+      propagate_at_launch = "true"
+    },
+    {
+      key                 = "Name"
+      value               = "terraform_asg_cluster"
+      propagate_at_launch = "true"
+    },
+    {
+      key                 = "BU"
+      value               = "group-testing"
+      propagate_at_launch = "true"
+    },
+    {
+      key                 = "Owner"
+      value               = "vagisha.jain@tothenew.com"
+      propagate_at_launch = "true"
+    },
+    {
+      key                 = "Purpose"
+      value               = "group project"
+      propagate_at_launch = "true"
+    }
+  ]
+}
